bda2872473f9d602fb3f01f6722bd38d701c74c0
Vitor França <franc.vitoor@gmail.com>
2021-09-08 20:40:51 -0300

    Companies controller

---
+++ src/controllers/CompaniesController.js
@@ -0,0 +1,126 @@
+const Companies = require('../models/Companies');
+
+module.exports = {
+  read: async (req, res) => {
+    try{
+      const { id } = req.params;
+
+      if(id){
+        const company = await Companies.findOne({ _id: id });
+
+        return res.json({
+          success: true,
+          company
+        });
+      }
+      else{
+        const companies = await Companies.find();
+
+        return res.json({
+            success: true,
+            companies
+        });
+      }
+    }catch(err){
+        res.json({
+            success: false,
+            error: err
+        });
+    };
+  },
+
+  create: async (req, res) => {
+    try{
+      if(req.body.password)
+        req.body.password = undefined;
+      
+      if(req.body.cnpj && await Companies.findOne({ cnpj: req.body.cnpj })) return res.json({
+        success: false,
+        error: 'CNPJ já cadastrado'
+      });
+      
+      const company = await Companies.create(req.body);
+
+      return res.json({
+        success: true,
+        company
+      });
+    }catch(err){
+        res.json({
+            success: false,
+            error: err
+        });
+    };
+
+  },
+
+  update: async (req, res) => {
+    try{
+        const { id } = req.params;
+
+        const company = await Companies.findByIdAndUpdate(id, req.body, { new: true });
+
+        return res.json({
+          success: true,
+          company
+        });
+    }catch(err){
+        return res.json({
+            success: false,
+            error: err
+        });
+    };
+
+  },
+
+  delete: async (req, res) => {
+    try{
+      const { id } = req.params;
+
+      await Companies.findOneAndRemove({ _id: id });
+
+      return res.json({
+        success: true
+      });
+
+    }catch(err){
+        res.json({
+            success: false,
+            error: err
+        });
+    };
+
+  },
+
+  registerPoint: async (req, res) => {
+    try{
+      const { date, hours, type, companyId } = req.body;
+      
+      if( !date || !hours || !type || !companyId) return res.json({
+        success: false,
+        error: 'Parâmetros inválidos'
+      });
+
+      const company = await Companies.findByIdAndUpdate(companyId, {
+        $push: {
+          points: {
+            date,
+            hours,
+            type
+          }
+        }
+      }, { new: true });
+
+      return res.json({
+        success: true,
+        company
+      });
+    } catch(err){
+        return res.json({
+            success: false,
+            error: err
+        });
+    };
+
+  },
+};
