797721dbf13164956a2cd10463c126cb3e147dfd
Vitor França <franc.vitoor@gmail.com>
2021-09-08 20:30:24 -0300

    Companies model

---
+++ src/models/Companies.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const CompaniesSchema = new mongoose.Schema({
+  cnpj: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  socialReason: {
+    type: String,
+    required: true,
+  },
+  fantasyName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  }
+});
+
+module.exports = mongoose.model('Companies', CompaniesSchema);
